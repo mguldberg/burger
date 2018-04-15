8f21b638b13d354182f82794522ea8713f5fc91b
mguldberg <guldberg@sbcglobal.net>
2018-04-15 13:39:44 -0500

    added connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 // Set up MySQL connection for burger app.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  port: 8889,
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db",
+    port: 8889,
+  });
+};
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
